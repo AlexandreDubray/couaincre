9253d90bc3fb1db86ee3f25c7d87591519ec4dd6
Alexandre Dubray <alexandre.dubray@uclouvain.be>
2026-02-19 17:51:52 +0100

    incremental computation of TD heuristic for speed-up

--- src/tree_decomposition.rs
+++ src/tree_decomposition.rs
@@ -2,7 +2,7 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 use clap::ValueEnum;
-use rustc_hash::FxHashSet;
+use rustc_hash::{FxHashSet, FxHashMap};
 
 use crate::Args;
 use crate::utils::*;
@@ -21,8 +21,11 @@ pub enum TDHeuristic {
 impl TreeDecomposition {
 
     pub fn new(args: &Args) -> Self {
+        println!("[Tree decomposition] Computing elimination order...");
         let number_var = number_var_from_dimacs(args.input.clone());
+        println!("[Tree decomposition] {} variables in the problem", number_var);
 
+        println!("[Tree decomposition] Creating the primal graph...");
         let mut primal_graph: Vec<FxHashSet<usize>> = (0..number_var).map(|_| FxHashSet::default()).collect();
 
         let reader = BufReader::new(File::open(args.input.clone()).unwrap());
@@ -42,22 +45,37 @@ impl TreeDecomposition {
             }
         }
 
+        println!("[Tree decomposition] Primal graph constructed. Computing elimination order...");
         // Computes the order of elimination for the tree-decomposition
         let mut width = 0;
         let mut order: Vec<usize> = vec![];
-        let mut candidates = (0..number_var).collect::<Vec<usize>>();
-        while !candidates.is_empty() {
-            let mut best = 0;
-            let mut best_score = usize::MAX;
-            for (index, candidate) in candidates.iter().copied().enumerate() {
-                let score = args.td_heuristic().evaluate_node(&primal_graph, candidate);
-                if score < best_score {
-                    best = index;
-                    best_score = score;
+        let mut buckets = vec![FxHashSet::<usize>::default(); args.td_heuristic().max_score(number_var) + 1];
+        let mut map_node_bucket = FxHashMap::<usize, usize>::default();
+        let mut recompute_score = vec![false; number_var];
+        let mut min_score = usize::MAX;
+        for candidate in 0..number_var {
+            let score = args.td_heuristic().evaluate_node(&primal_graph, candidate);
+            map_node_bucket.insert(candidate, score);
+            min_score = min_score.min(score);
+            buckets[score].insert(candidate);
+        }
+        while order.len() < number_var {
+            while buckets[min_score].is_empty() {
+                min_score += 1;
+            }
+            let node = *buckets[min_score].iter().next().unwrap();
+            if recompute_score[node] {
+                recompute_score[node] = false;
+                let new_score = args.td_heuristic().evaluate_node(&primal_graph, node);
+                if new_score > min_score {
+                    min_score = min_score.min(new_score);
+                    buckets[min_score].remove(&node);
+                    buckets[new_score].insert(node);
+                    map_node_bucket.insert(node, new_score);
+                    continue;
                 }
             }
-            let node = candidates[best];
-            candidates.swap_remove(best);
+            buckets[min_score].remove(&node);
             order.push(node);
             
             // Apply the node elimination, and compute the size of the created clique
@@ -70,8 +88,14 @@ impl TreeDecomposition {
                 primal_graph[neighbor].extend(neighbors.iter().copied().filter(|n| *n != neighbor));
                 primal_graph[neighbor].remove(&node);
             }
+            // Re-evaluate nodes for which the heuristic has changed
+            let nodes_to_update = args.td_heuristic().get_nodes_to_update(&primal_graph, node);
+            for n in nodes_to_update.iter().copied() {
+                recompute_score[n] = true;
+            }
             primal_graph[node].clear();
         }
+        debug_assert!(order.len() == order.iter().copied().collect::<FxHashSet<usize>>().len());
 
         Self {
             order,
@@ -90,7 +114,7 @@ impl TreeDecomposition {
 
 impl TDHeuristic {
 
-    fn evaluate_node(&self, graph: &Vec<FxHashSet<usize>>, node: usize) -> usize {
+    fn evaluate_node(&self, graph: &[FxHashSet<usize>], node: usize) -> usize {
         match self {
             Self::MinFill => {
                 if graph[node].is_empty() {
@@ -114,4 +138,30 @@ impl TDHeuristic {
         }
     }
 
+    fn get_nodes_to_update(&self, graph: &[FxHashSet<usize>], node: usize) -> Vec<usize> {
+        match self {
+            Self::MinFill => {
+                // All nodes at a distance of 2 in the graph can have their min-fill heuristic
+                // changed.
+                let mut to_update = FxHashSet::<usize>::default();
+                for neighbor in graph[node].iter().copied() {
+                    to_update.insert(neighbor);
+                    for neighbor_of_neighbor in graph[neighbor].iter().copied().filter(|n| *n != node) {
+                        to_update.insert(neighbor_of_neighbor);
+                    }
+                }
+                to_update.iter().copied().collect::<Vec<usize>>()
+            },
+            Self::MinDeg => {
+                graph[node].iter().copied().collect::<Vec<usize>>()
+            },
+        }
+    }
+
+    fn max_score(&self, number_var: usize) -> usize {
+        match self {
+            Self::MinFill => (number_var * (number_var - 1)) / 2,
+            Self::MinDeg => number_var,
+        }
+    }
 }
